79712b8ec7e1650bb474dbfff4068635a6633197
rbranjan47 <rbranjan47@gmail.com>
2022-07-11 00:56:39 +0530

    11th July

---
+++ my_Project/src/multiThreading/Java/demoMultiThreading.java
@@ -0,0 +1,38 @@
+/* Multi-Threading:
+ * Ways to define a Thread
+ *      - By extending thread class 
+ *      - By implementing runnable interface
+ *      
+ * Getting & Setting name of Thread
+ * 
+ * Thread Priorities
+ * 
+ * Methods:
+ *   - yield()
+ *   - join()
+ *   - sleep()
+ *   
+ *Synchronizatio
+ *
+ *Inter-Thread communication
+ *  - wait
+ *  - Notify
+ *  - Notify all
+ *  
+ * 
+ *Dead-lock
+ *
+ *Deamon Threads
+ *
+ *Multi-Threading Enhancements
+ *  - Thread Group
+ *  - Thread Local
+ *  - Executor Framework
+ */
+
+package multiThreading.Java;
+
+public class demoMultiThreading {
+
+	
+}
